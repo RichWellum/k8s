ee0c2cc262f33272c8fe9e20229d90887a8157d9
Rich Wellum <rwellum@lenovo.com>
2017-05-01 13:18:40 -0400

    damn

--- k8s.py
+++ k8s.py
@@ -153,15 +153,15 @@ def create_k8s_repo():
     repo = '/etc/yum.repos.d/kubernetes.repo'
     with open(name, "w") as w:
         w.write("""\
-        [kubernetes]
-        name=Kubernetes
-        baseurl=http://yum.kubernetes.io/repos/kubernetes-el7-x86_64
-        enabled=1
-        gpgcheck=0
-        repo_gpgcheck=1
-        gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
-        https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
-        """)
+ [kubernetes]
+ name=Kubernetes
+ baseurl=http://yum.kubernetes.io/repos/kubernetes-el7-x86_64
+ enabled=1
+ gpgcheck=0
+ repo_gpgcheck=1
+ gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
+ https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+ """)
     run(['sudo', 'mv', './kubernetes.repo', repo])
 
 
@@ -186,8 +186,6 @@ def main():
                 print("Found %s, Stopping and Disabling firewalld" % proc.name())
                 run(['sudo', 'systemctl', 'stop', 'firewalld'])
                 run(['sudo', 'systemctl', 'disable', 'firewalld'])
-            else:
-                logger.debug("firewalld not running")
 
         print("Install k8s 1.6.1 or later")
         create_k8s_repo()
