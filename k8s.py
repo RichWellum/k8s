d5b8b6cab67f4f16d918ac3d378957e809ecb3fd
Rich Wellum <rwellum@lenovo.com>
2017-05-01 08:55:15 -0400

    Initial work for k8s.py

---
+++ k8s.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+'''
+Author: Rich Wellum (richwellum@gmail.com)
+
+This is a tool to build a running Kubernetes Cluster on a single Bare Metal server
+or a VM running on a server.
+
+Initial supported state is a Centos 7 VM.
+
+Configuration is done with kubeadm.
+
+This relies heavily on my work on OpenStack kolla-kubernetes project and in
+particular the Bare Metal Deployment Guide:
+
+https://docs.openstack.org/developer/kolla-kubernetes/deployment-guide.html
+
+Inputs:
+
+1. build_vm : Build a centos 7 VM to run k8s on
+2. mgmt_int : Name of the interface to be used for management operations
+3. neutron_int: Name of the interface to be used for Neutron operations
+'''
+
+from __future__ import print_function
+import sys
+import os
+import time
+import subprocess
+import getpass
+import argparse
+from argparse import RawDescriptionHelpFormatter
+import re
+import logging
+import pexpect
+import tarfile
+
+
+__author__ = "Rich Wellum"
+__copyright__ = "Copyright 2017, Rich Wellum"
+__license__ = ""
+__version__ = "1.0.0"
+__maintainer__ = "Rich Wellum"
+__email__ = "rwellum@gmail.com"
+
+# Telnet ports used to access IOS XR via socat
+CONSOLE_PORT = 65000
+AUX_PORT = 65001
+
+# General-purpose retry interval and timeout value (10 minutes)
+RETRY_INTERVAL = 5
+TIMEOUT = 600
+
+logger = logging.getLogger(__name__)
+
+
+def set_logging():
+    '''
+    Set basic logging format.
+    '''
+    FORMAT = "[%(asctime)s.%(msecs)03d %(levelname)8s: %(funcName)20s:%(lineno)s] %(message)s"
+    logging.basicConfig(format=FORMAT, datefmt="%H:%M:%S")
+
+
+class AbortScriptException(Exception):
+    """Abort the script and clean up before exiting."""
+
+
+def run(cmd, hide_error=False, cont_on_error=False):
+    '''
+    Run command to execute CLI and catch errors and display them whether
+    in verbose mode or not.
+
+    Allow the ability to hide errors and also to continue on errors.
+    '''
+    s_cmd = ' '.join(cmd)
+    logger.debug("Command: '%s'\n", s_cmd)
+
+    output = subprocess.Popen(cmd,
+                              stdout=subprocess.PIPE,
+                              stderr=subprocess.PIPE)
+    tup_output = output.communicate()
+
+    if output.returncode != 0:
+        logger.debug('Command failed with code %d:', output.returncode)
+    else:
+        logger.debug('Command succeeded with code %d:', output.returncode)
+
+    logger.debug('Output for: ' + s_cmd)
+    logger.debug(tup_output[0])
+
+    if not hide_error and 0 != output.returncode:
+        logger.error('Error output for: ' + s_cmd)
+        logger.error(tup_output[1])
+        if not cont_on_error:
+            raise AbortScriptException(
+                "Command '{0}' failed with return code {1}".format(
+                    s_cmd, output.returncode))
+        logger.debug('Continuing despite error %d', output.returncode)
+
+    return tup_output[0]
+
+
+def start_process(args):
+    '''
+    Start vboxheadless process
+    '''
+    logger.debug('args: %s', args)
+    with open(os.devnull, 'w') as fp:
+        subprocess.Popen((args), stdout=fp)
+    time.sleep(2)
