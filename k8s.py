4917fa96817301a49f73a8cd657cc781128d8c1b
Rich Wellum <rwellum@lenovo.com>
2017-05-03 17:16:19 -0400

    Cleanups - works up to helm ini

--- k8s.py
+++ k8s.py
@@ -349,23 +349,12 @@ def main():
         print('Kubernetes - Installing k8s 1.6.1 or later - please wait')
         subprocess.check_output(
             'sudo yum install -y docker ebtables kubeadm kubectl kubelet kubernetes-cni git gcc', shell=True)
+
         print('Kubernetes - Enable the correct cgroup driver and disable CRI')
         run(['sudo', 'systemctl', 'enable', 'docker'])
         run(['sudo', 'systemctl', 'start', 'docker'])
-
-        # Looks like note needed?
-        # CGROUP_DRIVER = subprocess.check_output(
-        #     'sudo docker info | grep "Cgroup Driver" | awk "{print $3}"', shell=True)
-
-        # if 'systemd' in CGROUP_DRIVER:
-        #     # Todo: search and use
-        #     CGROUP_DRIVER = 'systemd'
-
         run(['sudo', 'cp', '/etc/systemd/system/kubelet.service.d/10-kubeadm.conf', '/tmp'])
         run(['sudo', 'chmod', '777', '/tmp/10-kubeadm.conf'])
-        # Latest version of canal doesn't need this
-        # run(['sudo', 'sed', '-i', 's|KUBELET_KUBECONFIG_ARGS=|KUBELET_KUBECONFIG_ARGS=--cgroup-driver=$CGROUP_DRIVER --enable-cri=false |g',
-        #      '/etc/systemd/system/kubelet.service.d/10-kubeadm.conf'])
 
         print('Kubernetes - Setup the DNS server with the service CIDR')
         run(['sudo', 'sed', '-i', 's/10.96.0.10/10.3.3.10/g', '/tmp/10-kubeadm.conf'])
@@ -393,9 +382,6 @@ def main():
         run(['sudo', 'sysctl', '-p'])
 
         print('Kubernetes - Deploying Kubernetes with kubeadm')
-        # kubeadm init --skip-preflight-checks --service-cidr 172.16.128.0/24 --pod-network-cidr 172.16.132.0/22 \
-        # --apiserver-advertise-address $(cat /etc/nodepool/primary_node_private) | tee /tmp/kubeout
-        # grep 'kubeadm join --token' /tmp/kubeout | awk '{print $4}' > /etc/kubernetes/token.txt
         run(['sudo', 'kubeadm', 'init', '--pod-network-cidr=10.1.0.0/16',
              '--service-cidr=10.3.3.0/24', '--skip-preflight-checks'])
 
