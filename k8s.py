753739cba83bfaae38f343e8bc1e6830d51140f9
Rich Wellum <rwellum@lenovo.com>
2017-05-04 07:12:58 -0400

    regexp

--- k8s.py
+++ k8s.py
@@ -370,10 +370,11 @@ def k8_fix_iptables():
 def k8s_fix_bridging():
     reload_sysctl = False
     with open('/tmp/sysctl.conf', 'a') as myfile:
-        if not re.search("net.bridge.bridge-nf-call-ip6tables=1", myfile):
+        contents = myfile.read()
+        if not re.search('net.bridge.bridge-nf-call-ip6tables=1', contents):
             myfile.write('net.bridge.bridge-nf-call-ip6tables=1' + '\n')
             reload_sysctl = True
-        if not re.search("net.bridge.bridge-nf-call-iptables=1", myfile):
+        if not re.search('net.bridge.bridge-nf-call-iptables=1', contents):
             myfile.write('net.bridge.bridge-nf-call-iptables=1' + '\n')
             reload_sysctl = True
     if reload_sysctl is True:
