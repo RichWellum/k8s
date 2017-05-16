8d83a19396de95715699f4dd5035e96c90a6e9b1
Rich Wellum <rwellum@lenovo.com>
2017-05-16 17:56:30 -0400

    Indent

--- k8s.py
+++ k8s.py
@@ -710,30 +710,31 @@ def reset_sudo_timeout():
 
 
 def kolla_create_keystone():
-    '''   Using OpenStack
+    '''Using OpenStack
 
-   If you were able to successfully reach the end of this guide and demo1 was
-   successfully deployed, here is a fun list of things you can do
-   with your new cluster.
+    If you were able to successfully reach the end of this guide and demo1 was
+    successfully deployed, here is a fun list of things you can do
+    with your new cluster.
 
-   Access Horizon GUI
+    Access Horizon GUI
 
-   1 Determine Horizon EXTERNAL IP Address:
+    1 Determine Horizon EXTERNAL IP Address:
 
-   $ kubectl get svc horizon --namespace=kolla
-   NAME      CLUSTER-IP   EXTERNAL-IP     PORT(S)   AGE
-   horizon   10.3.3.237   10.240.43.175   80/TCP    1d
+    $ kubectl get svc horizon --namespace=kolla
+    NAME      CLUSTER-IP   EXTERNAL-IP     PORT(S)   AGE
+    horizon   10.3.3.237   10.240.43.175   80/TCP    1d
 
-   2 Determine username and password from keystone:
+    2 Determine username and password from keystone:
 
-   $ cat ~/keystonerc_admin | grep OS_USERNAME
-   export OS_USERNAME=admin
+    $ cat ~/keystonerc_admin | grep OS_USERNAME
+    export OS_USERNAME=admin
 
-   $ cat ~/keystonerc_admin | grep OS_PASSWORD
-   export OS_PASSWORD=Sr6XMFXvbvxQCJ3Cib1xb0gZ3lOtBOD8FCxOcodU
+    $ cat ~/keystonerc_admin | grep OS_PASSWORD
+    export OS_PASSWORD=Sr6XMFXvbvxQCJ3Cib1xb0gZ3lOtBOD8FCxOcodU
 
-   3 Run a browser that has access to your network, and access Horizon GUI with
-   the EXTERNAL IP from Step 1, using the credentials from Step 2.'''
+    3 Run a browser # TODO: hat has access to your network, and access Horizon GUI with
+    the EXTERNAL IP from Step 1, using the credentials from Step 2.'''
+
     run_shell('sudo rm ~/keystonerc_admin')
     run_shell('kolla-kubernetes/tools/build_local_admin_keystonerc.sh')
     address = run_shell('kubectl get svc horizon --namespace=kolla --no-headers | cut -d' ' -f7')
