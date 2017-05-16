e03dfa797ac4cf3017737aee9bb7d3213264199d
Rich Wellum <rwellum@lenovo.com>
2017-05-16 12:16:27 -0400

    1.6.3

--- k8s.py
+++ k8s.py
@@ -283,7 +283,7 @@ def k8s_create_repo():
     # run_shell(
     #     'sudo yum install -y docker ebtables kubeadm-1.6.2 kubectl-1.6.2 kubelet-1.6.2 kubernetes-1.5.2-0.2 git gcc')
     run_shell(
-        'sudo yum install -y docker ebtables kubeadm kubectl kubernetes git gcc')
+        'sudo yum install -y docker ebtables kubeadm-1.6.3 kubectl-1.6.3 kubernetes-1.5.2-0.2 git gcc')
     # Workaround until kubectl 1.6.4 is available
     run_shell('sudo yum remove kubectl -y')
     run_shell('sudo curl -L  https://github.com/sbezverk/kubelet--45613/raw/master/kubelet.gz | sudo gzip -d > /usr/bin/kubelet')
