9463e66e2486179ad1348726a7cc122f91db1a23
rwellum <rwellum@lenovo.com>
2017-06-19 16:33:07 -0400

    df

--- k8s.py
+++ k8s.py
@@ -391,7 +391,7 @@ def k8s_install_tools(a_ver, j_ver):
         run_shell('sudo yum install -y git crudini jq ansible')
     else:
         run_shell(
-            'sudo apt update && sudo yum upgrade -y && sudo apt \
+            'sudo apt update && sudo apt upgrade -y && sudo apt \
             full-upgrade -y && sudo apt autoremove -y && sudo apt autoclean')
         run_shell('sudo apt-get install -y bridge-utils nmap')
         run_shell('sudo apt-get install -y python-dev libffi-dev gcc libssl-dev \
