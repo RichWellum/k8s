a172652ac6fa3e0d77ee94815870af9d19433489
rwellum <rwellum@lenovo.com>
2017-06-22 14:34:48 -0400

    Remove kolla from create commands

--- k8s.py
+++ k8s.py
@@ -1012,7 +1012,7 @@ def kolla_gen_secrets():
     demo('Create secrets from the generated password file using "kubectl create secret generic"',
          'Kubernetes Secrets is an object that contains a small amount of\n' +
          'sensitive data such as passwords, keys and tokens etc')
-    run_shell('python ./kolla-kubernetes/tools/secret-generator.py create kolla')
+    run_shell('python ./kolla-kubernetes/tools/secret-generator.py create')
 
 
 def kolla_create_config_maps():
