d12221ae08ed2d41a31baac3f4fa0e78ce81e6f2
rwellum <rwellum@lenovo.com>
2017-07-17 22:46:52 -0400

    Slightly better success message

--- ko.py
+++ ko.py
@@ -1601,7 +1601,8 @@ done
         "kubectl get svc horizon --namespace kolla --no-headers | awk '{print $3}'")
     username = run_shell("cat ~/keystonerc_admin | grep OS_PASSWORD | awk '{print $2}'")
     password = run_shell("cat ~/keystonerc_admin | grep OS_USERNAME | awk '{print $2}'")
-    print_progress('Kolla', 'To Access Horizon:', KOLLA_FINAL_PROGRESS)
+    print_progress('Kolla', 'Complete Kolla OpenStack Cluster! To Access Horizon:',
+                   KOLLA_FINAL_PROGRESS)
 
     print('  Point your browser to: %s' % address)
     print('  %s' % username)
