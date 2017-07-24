1c76fd1d1a9d035488deb82cfe136f29754f76f6
rwellum <rwellum@lenovo.com>
2017-07-24 15:33:42 -0400

    df1

--- ko.py
+++ ko.py
@@ -498,13 +498,13 @@ def populate_ip_addresses(args):
         find_vip = '/tmp/find_vip'
         with open(find_vip, "w") as w:
             w.write("""\
-for i in {2..253}; do
-   sudo nmap -sP -PR 10.240.43.2 | grep -i "Host seems down" >/dev/null;
-   if [ $? -ne 0 ]; then
-         echo "%s.$i";
-         break;
-   fi;
-done
+    for i in {2..253}; do
+        sudo nmap -sP -PR 10.240.43.2 | grep -i "Host seems down" >/dev/null;
+        if [ $? -ne 0 ]; then
+          echo "%s.$i";
+          break;
+        fi;
+    done
             """ % (start_ip, start_ip))
         vip = run_shell('sudo bash %s' % find_vip)
         args.vip_ip = vip.strip()
