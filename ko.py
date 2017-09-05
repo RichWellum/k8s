eddb49c2af1053e2bffb90d9fed4f9e32670828c
rwellum <rwellum@lenovo.com>
2017-09-05 16:35:50 -0400

    df

--- ko.py
+++ ko.py
@@ -2078,7 +2078,7 @@ def kolla_get_neutron_subnet(args):
     '''Find and return a neutron ip address that can be used for a
     floating ip the neutron subnet'''
     print('DEBUG: int %s' % args.NEUTRON_INT)
-    out = run_shell(args, 'sudo dhclient %s -v -r' % args.NEUTRON_INT)
+    print(run_shell(args, 'sudo dhclient %s -v -r' % args.NEUTRON_INT))
     print('DEBUGXX "%s"' % out)
     run_shell(args, 'echo %s > /tmp/dhcp' % out)
     out2 = run_shell(
