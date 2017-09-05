f146d6285660d6b807dc80e11e69b80f099ae4db
rwellum <rwellum@lenovo.com>
2017-09-05 16:50:38 -0400

    df

--- ko.py
+++ ko.py
@@ -2079,8 +2079,9 @@ def kolla_get_neutron_subnet(args):
     floating ip the neutron subnet'''
     print('DEBUG: int %s' % args.NEUTRON_INT)
     # print(run_shell(args, 'sudo dhclient %s -v -r' % args.NEUTRON_INT))
-    output = subprocess.Popen("sudo dhclient %s -v -r",
-                              stdout=subprocess.PIPE)
+    output = subprocess.Popen("sudo dhclient %s -v -r" % args.NEUTRON_INT,
+                              stdout=subprocess.PIPE,
+                              stderr=subprocess.PIPE)
     tup_output = output.communicate()
     print(tup_output)
     # print(
