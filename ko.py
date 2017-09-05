52cba955c460d43b3767483f7aaa83164462a901
rwellum <rwellum@lenovo.com>
2017-09-05 17:06:04 -0400

    df

--- ko.py
+++ ko.py
@@ -2077,33 +2077,14 @@ done
 def kolla_get_neutron_subnet(args):
     '''Find and return a neutron ip address that can be used for a
     floating ip the neutron subnet'''
-    print('DEBUG: int %s' % args.NEUTRON_INT)
     run_shell(
         args,
         'sudo dhclient %s -v -r > /tmp/dhcp 2>&1' %
         args.NEUTRON_INT)
-    # print(run_shell(args, 'sudo dhclient %s -v -r' % args.NEUTRON_INT))
-    # print(
-    #     subprocess.Popen(
-    #         "sudo dhclient %s -v -r >& /tmp/dhcp" %
-    #         args.NEUTRON_INT,
-    #         shell=True,
-    #         stdout=subprocess.PIPE).stdout.read())
 
-    # print('DEBUGXX "%s"' % out)
-    # run_shell(args, 'echo %s > /tmp/dhcp' % out)
-    out2 = run_shell(
-        args,
-        "cat /tmp/dhcp | grep DHCPRELEASE | awk '{ print $5 }'")
-    print('DEBUG2: out2 %s' % out2)
-    run_shell(args, 'sudo cat /tmp/dhcp')
-    print('DEBUG3')
-    time.sleep(1500)
-    sys.exit(1)
     out = run_shell(
         args,
         "cat /tmp/dhcp | grep DHCPRELEASE | awk '{ print $5 }'")
-    print('DEBUG out %s' % out)
     subnet = out[:out.rfind(".")]
     print('DEBUG subnet %s' % subnet)
     r = list(range(2, 253))
