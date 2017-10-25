38d76284e6ed12020faabbc230a1f761e6d33c21
rwellum <rwellum@lenovo.com>
2017-10-24 20:01:24 -0400

    Remove ocata hack

--- ko.py
+++ ko.py
@@ -2583,7 +2583,7 @@ def kolla_bring_up_openstack(args):
     kolla_build_micro_charts(args)
     kolla_verify_helm_images(args)
 
-    if 'ocataaaa' in args.image_version:
+    if 'ocata' in args.image_version:
         kolla_create_cloud_v4(args)
     else:
         kolla_create_cloud(args)
@@ -2592,7 +2592,7 @@ def kolla_bring_up_openstack(args):
     # dockerhub have to run them from a docker registry running as a pod.
     # This takes a long time to come up but then all the other image
     # pulls are very quick.
-    if 'ocataaaaaa' not in args.image_version:  # todo remove speling
+    if 'ocata' not in args.image_version:  # todo remove speling
         banner(
             'Installing docker registry. Slow but needed for 5.x as '
             'images are not on dockerhub yet.')
