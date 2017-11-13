f6ff96e06569079555e6cada1ca1c2263b7ffb00
rwellum <rwellum@lenovo.com>
2017-11-13 15:25:12 -0500

    More kubectl env fixes

--- ko.py
+++ ko.py
@@ -893,16 +893,14 @@ def k8s_setup_dns(args):
     run_shell(args, 'sudo chmod 777 /tmp/10-kubeadm.conf')
     run_shell(args,
               'sudo sed -i s/10.96.0.10/10.3.3.10/g /tmp/10-kubeadm.conf')
+    run_shell(args,
+              'sudo echo "Environment="KUBELET_CGROUP_ARGS=--cgroup-driver=systemd" '
+              '>> /tmp/10-kubeadm.conf')
+    run_shell(args,
+              'sudo echo "Environment="KUBELET_EXTRA_ARGS=--fail-swap-on=false" '
+              '>> /tmp/10-kubeadm.conf')
     run_shell(args, 'sudo mv /tmp/10-kubeadm.conf '
               '/etc/systemd/system/kubelet.service.d/10-kubeadm.conf')
-    run_shell(
-        args,
-        'sudo echo "Environment="KUBELET_CGROUP_ARGS=--cgroup-driver=systemd" '
-        '>> /tmp/10-kubeadm.conf')
-    run_shell(
-        args,
-        'sudo echo "Environment="KUBELET_EXTRA_ARGS=--fail-swap-on=false" '
-        '>> /tmp/10-kubeadm.conf')
 
     # https://github.com/kubernetes/kubernetes/issues/53333#issuecomment-339793601
 #     ftc_c = '/tmp/90-local-extras.conf'
