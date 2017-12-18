6039cdc5889aa77d97f80653b57af1670b5d7389
rwellum <rwellum@lenovo.com>
2017-12-18 17:09:00 -0500

    cinder wip

--- ko.py
+++ ko.py
@@ -1896,7 +1896,7 @@ storwize_svc_volpool_name = Pool0
     run_shell(args,
               'cd ./kolla-kubernetes; '
               'git fetch git://git.openstack.org/openstack/kolla-kubernetes '
-              'refs/changes/24/528724/6 && git cherry-pick FETCH_HEAD')
+              'refs/changes/24/528724/7 && git cherry-pick FETCH_HEAD')
 
     # add v3 end points
 #     l1 = 'cinder_v3_admin_endpoint: "{{ admin_protocol }}://' \
