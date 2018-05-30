cbffe702b4c72d6074df386a5fb2e7ecb8aba0b7
Rich Wellum <richwellum@gmail.com>
2018-05-30 15:29:44 -0400

    Niggles

--- k8s.py
+++ k8s.py
@@ -1163,10 +1163,10 @@ def k8s_install_deploy_helm(args):
             print_progress('Kubernetes',
                            'Helm successfully installed',
                            K8S_FINAL_PROGRESS)
-            print_progress('Kubernetes',
-                           'Updating Helm Repo',
-                           K8S_FINAL_PROGRESS)
-            run_shell(args, 'helm repo update')
+            # print_progress('Kubernetes',
+            #                'Updating Helm Repo',
+            #                K8S_FINAL_PROGRESS)
+            # run_shell(args, 'helm repo update')
             run_shell(args, 'kubectl get pods --all-namespaces')
             print('  You can now join any number of machines by '
                   'running the following on each node as root:')
