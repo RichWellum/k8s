686e72384bb5c948123972f6aeef5a6224069fcb
Rich Wellum <richwellum@gmail.com>
2018-06-04 11:08:35 -0400

    Typo

--- k8s.py
+++ k8s.py
@@ -41,7 +41,7 @@ TODO
 ====
 
 1. Convert to using https://github.com/kubernetes-incubator/client-python
-2. Classigy the code
+2. Classify the code
 3. break the unix output down into same style
 4. Note there are various todo's scattered inline as well.
 
