4fe8de303d9338ee3837020c5d32f9b51ba226e5
Rich Wellum <richwellum@gmail.com>
2018-06-19 20:45:04 -0400

    correct wd

--- osh.sh
+++ osh.sh
@@ -34,24 +34,24 @@ kubectl label nodes osh ceph-mds=enabled
 kubectl label nodes osh ceph-rgw=enabled
 kubectl label nodes osh ceph-mgr=enabled
 
-./tools/deployment/developer/common/020-setup-client.sh
-./tools/deployment/developer/common/030-ingress.sh
-./tools/deployment/developer/ceph/040-ceph.sh
-./tools/deployment/developer/ceph/045-ceph-ns-activate.sh
-./tools/deployment/developer/ceph/050-mariadb.sh
-./tools/deployment/developer/ceph/060-rabbitmq.sh
-./tools/deployment/developer/ceph/070-memcached.sh
-./tools/deployment/developer/ceph/080-keystone.sh
-./tools/deployment/developer/ceph/090-heat.sh
-./tools/deployment/developer/ceph/100-horizon.sh
-./tools/deployment/developer/ceph/110-ceph-radosgateway.sh
-./tools/deployment/developer/ceph/120-glance.sh
-./tools/deployment/developer/ceph/130-cinder.sh
-./tools/deployment/developer/ceph/140-openvswitch.sh
-./tools/deployment/developer/ceph/150-libvirt.sh
-./tools/deployment/developer/ceph/160-compute-kit.sh
-./tools/deployment/developer/ceph/170-setup-gateway.sh
-./tools/deployment/developer/common/900-use-it.sh
+./openstack-helm/tools/deployment/developer/common/020-setup-client.sh
+./openstack-helm/tools/deployment/developer/common/030-ingress.sh
+./openstack-helm/tools/deployment/developer/ceph/040-ceph.sh
+./openstack-helm/tools/deployment/developer/ceph/045-ceph-ns-activate.sh
+./openstack-helm/tools/deployment/developer/ceph/050-mariadb.sh
+./openstack-helm/tools/deployment/developer/ceph/060-rabbitmq.sh
+./openstack-helm/tools/deployment/developer/ceph/070-memcached.sh
+./openstack-helm/tools/deployment/developer/ceph/080-keystone.sh
+./openstack-helm/tools/deployment/developer/ceph/090-heat.sh
+./openstack-helm/tools/deployment/developer/ceph/100-horizon.sh
+./openstack-helm/tools/deployment/developer/ceph/110-ceph-radosgateway.sh
+./openstack-helm/tools/deployment/developer/ceph/120-glance.sh
+./openstack-helm/tools/deployment/developer/ceph/130-cinder.sh
+./openstack-helm/tools/deployment/developer/ceph/140-openvswitch.sh
+./openstack-helm/tools/deployment/developer/ceph/150-libvirt.sh
+./openstack-helm/tools/deployment/developer/ceph/160-compute-kit.sh
+./openstack-helm/tools/deployment/developer/ceph/170-setup-gateway.sh
+./openstack-helm/tools/deployment/developer/common/900-use-it.sh
 export OS_CLOUD=openstack_helm
 
 
