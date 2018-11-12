7bc65eb8c6122e4f6b0b0847904091e488ebe010
Rich Wellum <richwellum@gmail.com>
2018-11-12 18:00:32 -0500

    docker work

--- k8s.py
+++ k8s.py
@@ -612,7 +612,7 @@ def k8s_install_tools(args):
                       'https://download.docker.com/linux/centos/'
                       'docker-ce.repo')
             run_shell(args,
-                      'sudo yum install docker-ce -y')
+                      'sudo yum install docker-ce-18.06.1.ce -y')
         else:
             # ubuntu
             # https://docs.docker.com/install/linux/docker-ce/ubuntu/#install-
