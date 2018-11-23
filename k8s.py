7d344a725529036a2088ff07eb1862dc365e8c17
Rich Wellum <richwellum@gmail.com>
2018-11-23 17:13:11 +0100

    blah

--- k8s.py
+++ k8s.py
@@ -1027,7 +1027,7 @@ def k8s_final_messages(args):
 
     banner("Kuberntes Join")
     print('You can now join any number of Nodes by running the following on '
-          'each node as root"')
+          'each node as root:')
     print('1. Set up Node as a minion: "k8s.py -cm"')
     print('2. "sudo %s"' % JOIN_CMD.lstrip())
     print('3. Join command is also saved here: "%s"' % output_file_name)
